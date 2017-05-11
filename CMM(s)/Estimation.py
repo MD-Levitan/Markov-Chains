1507eab3ebf71bba40d4e264bfa73f8dc2b04d2a
MD_Levitan <starmaksim97@gmail.com>
2017-05-11 03:36:52 +0200

    fix Estimation

--- CMM(s)/Estimation.py
+++ CMM(s)/Estimation.py
@@ -12,10 +12,14 @@ class Estimation:
 
         def __init__(self, model, l_max=1000, k_max=1000):
             self.model = model
+            self.alg = 'mle'
             self.L_max = l_max
             self.K_max = k_max
             self.sample = Estimation.generate_sample(self.model, self.L_max,
                                                     self.K_max)
+
+        def __set_alg__(self, value):
+            alg = value
 
         @staticmethod
         def generate_sample(model, l_max, k_max):
@@ -27,10 +31,10 @@ class Estimation:
         def norm(ar1, ar2):
             return np.linalg.norm((ar1 - ar2), ord='fro')
 
-        def standard_deviation(self, l, k, param='P',):
+        def standard_deviation(self, l, k, param='P'):
             if 0 <= k < self.K_max and 0 <= l < self.L_max - self.model.s - 1:
                 print(str(k) + " " + str(l) + "\n")
-                estimation_model = alg.estimation_model(self.sample[l][k], self.model, alg='mle')
+                estimation_model = alg.estimation_model(self.sample[l][k], self.model, alg=self.alg)
                 if param == 'P':
                     std_deviation = Estimation.norm(estimation_model.P, self.model.P)
                 if param == 'Pi':
@@ -46,7 +50,7 @@ class Estimation:
             std = [self.estimation_deviation(l) for l in range(0, self.L_max - self.model.s - 1)]
             max_value = math.ceil(max(std))
             step_y = max_value / 20
-            step_x = 5
+            step_x = self.L_max / 20
             fig, ax = plt.subplots()
             plt.title("")
 
@@ -60,7 +64,8 @@ class Estimation:
             plt.grid()
             plt.show()
 
-cmm = CMM_S(2, 2)
-est = Estimation(cmm, 80, 1000)
+cmm = CMM_S(10, 2)
+est = Estimation(cmm, 280, 100)
+est.__set_alg__('mle')
 est.graphic()
 
--- CMM/Estimation.py
+++ CMM/Estimation.py
@@ -11,6 +11,7 @@ class Estimation:
 
         def __init__(self, model, l_max=1000, k_max=1000):
             self.model = model
+            self.alg = 'mle'
             self.L_max = l_max
             self.K_max = k_max
             self.sample = Estimation.generate_sample(self.model, self.L_max,
@@ -22,14 +23,18 @@ class Estimation:
                       for l in range(1, l_max)]
             return sample
 
+        def __set_alg__(self, value):
+            alg = value
+
+
         @staticmethod
         def norm(ar1, ar2):
             return np.linalg.norm((ar1 - ar2), ord='fro')
 
-        def standard_deviation(self, l, k, param='P',):
-            if 0 <= k < self.K_max and 0 <= l < self.L_max - 1:
+        def standard_deviation(self, l, k, param='P'):
+            if 0 <= k < self.K_max and 0 <= l < self.L_max - self.model.s - 1:
                 print(str(k) + " " + str(l) + "\n")
-                estimation_model = alg.estimation_model(self.sample[l][k], self.model, alg='mle')
+                estimation_model = alg.estimation_model(self.sample[l][k], self.model, alg=self.alg)
                 if param == 'P':
                     std_deviation = Estimation.norm(estimation_model.P, self.model.P)
                 if param == 'Pi':
@@ -42,16 +47,16 @@ class Estimation:
 
         def graphic(self):
             import math
-            std = [self.estimation_deviation(l) for l in range(0, self.L_max - 1)]
+            std = [self.estimation_deviation(l) for l in range(0, self.L_max - self.model.s - 1)]
             max_value = math.ceil(max(std))
             step_y = max_value / 20
-            step_x = 5
+            step_x = self.L_max / 20
             fig, ax = plt.subplots()
             plt.title("")
 
-            ax.plot(range(1, self.L_max), std)
+            ax.plot(range(self.model.s + 1, self.L_max), std)
 
-            ax.set_xticks(np.arange(1, self.L_max, step_x))
+            ax.set_xticks(np.arange(self.model.s + 1, self.L_max, step_x))
             ax.set_yticks(np.arange(0, max_value, step_y))
 
             ax.set_xlabel("T")
